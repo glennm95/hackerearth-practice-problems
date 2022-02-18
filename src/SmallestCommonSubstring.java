5b7a79a5a1d7e39d90685058456ca60e00ff252e
Glenn Mascarenhas <glennmascarenhas24395@gmail.com>
2022-02-18 20:48:17 +0530

    - So apparently dynamic programming was completely unnecessary here! The answer is simple and straightforward if you understand the question well. Any smallest common substring(s) between two strings, if present, will have length 1, since even if a common substring of length 2 or more is present, that would mean that each of the single characters in the substrings is common and therefore the smallest length is always 1! - Came to this conclusion after digging deep into number of minimum calls that would be needed to find all distinct substrings of a string and that value for a string of size 10^5 is at least 2^(10^5)+ which is impossible to run. Therefore, there had to be an alternate solution, and it turns out that we didn't need to find all distinct substrings at all! Just two linear time for loops, one for looping through characters (since char length of 1 is min) in B and storing them in HashMap and other for looping through A and doing a constant lookup in the HashMap. Therefore, the code ends up having O(n) time complexity where n is max(|A|,|B|)

--- src/SmallestCommonSubstring.java
+++ src/SmallestCommonSubstring.java
@@ -1,49 +1,78 @@
-import java.util.ArrayList;
+/*
+Smallest Common Substrings
+
+Question
+
+You are given two strings A and B that are made of lowercase English alphabets. Find the number of different pairs
+((i, j), (k,l)) such that the substrings A[i...j] andB[k...l] are equal and the value of j - i + 1 is minimum.
+
+Input Format
+
+The first line consists of a string denoting A.
+The second line consists f a string denoting B.
+
+Output Format
+
+Print the number of different pairs ((i, j), (k,l)) such that the substrings A[i...j] and B[k...l] are equal and the value of j-i+1 is minimum.
+
+Constraints
+
+1<=|A|, |B|<=10^5
+
+Sample Input
+abdc
+bd
+
+Sample Output
+2
+
+Explanation
+Pairs are ((1, 1), (0, 0)) and ((2, 2), (1, 1))
+
+ */
+
+
 import java.util.HashMap;
+import java.util.Random;
 
 
-public class SmallestCommonSubstring {
-    public static void nSmallestCommonSubstring(String A, String B, HashMap<String, Boolean> map){
-
-        map.put(A, B.contains(A));
-
-        if(A.length() == 1)
-            return;
-
-        StringBuffer subA;
-
-        for(int i=0; i<A.length(); i++){
-            subA = new StringBuffer(A).deleteCharAt(i);
-            if(!map.containsKey(subA.toString()))
-                nSmallestCommonSubstring(subA.toString(), B, map);
-        }
-
-    }
+public class SmallestCommonSubstring{
 
     public static void main(String[] args) {
         HashMap<String, Boolean> map = new HashMap<>();
 
-        nSmallestCommonSubstring("abcd", "bc", map);
+        // generate random Strings from pool of lowercase English characters
+        String alphas = "abcdefghijklmnopqrstuvwxyz";
 
-        int min_size = 999999;
+        Random r = new Random();
+        StringBuilder A = new StringBuilder(100);
+        StringBuilder B = new StringBuilder(100);
 
-        for(String x: map.keySet())
-            if(map.get(x) && x.length() < min_size)
-                min_size = x.length();
+        for(int i=0; i<100000; i++){
+            A.append(alphas.charAt(r.nextInt(26)));
+            B.append(alphas.charAt(r.nextInt(26)));
+        }
 
-        int num = 0;
 
-        ArrayList<String> subs = new ArrayList<>();
+        // store each character in String B in HashMap; Boolean initialized to false
+        for(String x: B.toString().split("")){
+            if(!map.containsKey(x))
+                map.put(x, false);
+        }
 
-        for(String x: map.keySet())
-            if(x.length() == min_size && map.get(x)) {
-                subs.add(x);
-                num += 1;
+        int count = 0;      // to keep track of total distinct characters
+
+        // loop through characters of String A and check if character is present in HashMap => character is in B
+        for(String x: A.toString().split("")){
+            if(map.containsKey(x) && !map.get(x)) {     // second condition ensures that String is checked for
+                                                        // membership in HashMap only once
+                                                        // !map.get(x) => character is not visited before
+                count += 1;
+                map.replace(x, true);                   // mark character as visited
             }
+        }
 
-//        System.out.println(map);
-        System.out.println(subs);
-
-        System.out.println(num);
+        System.out.println(count);      // output the total number of distinct characters..
+                                        // max count value will be 26 for 26 alphabets
     }
 }
