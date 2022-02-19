c15979ff549248f8ea4c0894e845d05b8f7cf684
Glenn Mascarenhas <glennmascarenhas24395@gmail.com>
2022-02-19 09:17:46 +0530

    - A slight flaw in the previous version was spotted after reading the problem statement again carefully. The problem asks for number of different pairs of ((i,j),(k,l)) where i,j are index positions in A and k,l are index positions in B and not for unique single character occurrences like I've output in the previous commit. - If 'c' is present in A at positions 0,3 and 'c' is present in B at positions 3,7 then the total # of different pairs of ((i,j),(k,l)) will be 2*2 = 4 viz. ((0,0),(3,3)), ((0,0),(7,7)), ((3,3),(3,3)), ((3,3),(7,7)). This should be the correct output rather than outputting only 1 for the common 'c'.

--- src/SmallestCommonSubstring.java
+++ src/SmallestCommonSubstring.java
@@ -31,6 +31,8 @@ Pairs are ((1, 1), (0, 0)) and ((2, 2), (1, 1))
 
  */
 
+// need to push code to git and answer corresponding leetcode query online
+
 
 import java.util.HashMap;
 import java.util.Random;
@@ -39,7 +41,8 @@ import java.util.Random;
 public class SmallestCommonSubstring{
 
     public static void main(String[] args) {
-        HashMap<String, Boolean> map = new HashMap<>();
+        HashMap<Character, Integer> mapA = new HashMap<>();     // HashMap from character to # times the
+        HashMap<Character, Integer> mapB = new HashMap<>();     // character appears in the String
 
         // generate random Strings from pool of lowercase English characters
         String alphas = "abcdefghijklmnopqrstuvwxyz";
@@ -53,26 +56,44 @@ public class SmallestCommonSubstring{
             B.append(alphas.charAt(r.nextInt(26)));
         }
 
+//        StringBuilder A = new StringBuilder("abcd");
+//        StringBuilder B = new StringBuilder("bc");
 
-        // store each character in String B in HashMap; Boolean initialized to false
-        for(String x: B.toString().split("")){
-            if(!map.containsKey(x))
-                map.put(x, false);
+//        System.out.println(A);
+//        System.out.println(B);
+
+        // Add characters from A into HashMap
+        for(int i=0; i<A.length(); i++){
+            char curr = A.charAt(i);
+            if(!mapA.containsKey(curr))
+                mapA.put(curr, 1);                          // character appears once in the String (first encounter)
+            else
+                mapA.replace(curr, mapA.get(curr)+1);       // character encountered again, increment map value by 1
         }
 
-        int count = 0;      // to keep track of total distinct characters
-
-        // loop through characters of String A and check if character is present in HashMap => character is in B
-        for(String x: A.toString().split("")){
-            if(map.containsKey(x) && !map.get(x)) {     // second condition ensures that String is checked for
-                                                        // membership in HashMap only once
-                                                        // !map.get(x) => character is not visited before
-                count += 1;
-                map.replace(x, true);                   // mark character as visited
-            }
+        // Add characters from B into HashMap
+        for(int i=0; i<B.length(); i++){
+            char curr = B.charAt(i);
+            if(!mapB.containsKey(curr))
+                mapB.put(curr, 1);
+            else
+                mapB.replace(curr, mapB.get(curr)+1);
         }
 
-        System.out.println(count);      // output the total number of distinct characters..
-                                        // max count value will be 26 for 26 alphabets
+//        System.out.println(mapA);
+//        System.out.println(mapB);
+
+        int count = 0;
+
+        for(char z: mapA.keySet()){
+            if(mapB.containsKey(z)){
+                count += mapA.get(z) * mapB.get(z);         // total number of different pairs of ((i,j),(k,l)) will be
+            }                                               // (# of positions z is present in A) * (# of positions z
+        }                                                   // is present in B) Eg. if 'c' is present in A 3 times
+                                                            // & 'c' is present in B 4 times then the total #
+                                                            // ((i,j),(k,l)) pairs are 3*4=12
+        System.out.println(count);
+
+        
     }
 }
